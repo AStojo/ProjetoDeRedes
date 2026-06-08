0a4dee12251194f60265e26fabd70b0ae940a2ce
acpsoutto <acpsoutto@gmail.com>
2026-06-08 19:15:15 +0100

    [ATL] - atualização de codigo

--- uploads/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-::ñ mostra os comando no ecrã, só o output
-cd bin    
-::entra na pasta bin onde estão os .class compilados
-java server.Servidor 
-::corre o servidor 
-pause     
-::mantem a janela aberta para ver o output
